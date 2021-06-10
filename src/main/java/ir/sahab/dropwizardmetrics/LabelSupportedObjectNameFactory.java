63043ccc45742ef1f67962dfc8987ee0755a12b8
MSDehghan <msdehghan77@gmail.com>
2021-06-10 17:58:23 +0430

    Add public hasLabel and extractLabels methods

--- src/main/java/ir/sahab/dropwizardmetrics/LabelSupportedObjectNameFactory.java
+++ src/main/java/ir/sahab/dropwizardmetrics/LabelSupportedObjectNameFactory.java
@@ -3,6 +3,7 @@ package ir.sahab.dropwizardmetrics;
 import com.codahale.metrics.jmx.ObjectNameFactory;
 import javax.management.MalformedObjectNameException;
 import javax.management.ObjectName;
+import java.util.Map;
 
 /**
  * It is a custom implementation of {@code ObjectNameFactory} which supports exposing metric labels in JMX.
@@ -46,8 +47,7 @@ public class LabelSupportedObjectNameFactory implements ObjectNameFactory {
     @Override
     public ObjectName createName(String type, String domain, String labeledMetricName) {
         String metricName = extractMetricName(labeledMetricName);
-        String labels = extractLabels(labeledMetricName);
-        String quotedLabels = quoteLabelValues(labels);
+        String quotedLabels = extractQuotedLabelValues(labeledMetricName);
 
         String name = quoteDomainIfRequired(domain) + ":name=" + quoteValueIfRequired(metricName) + quotedLabels;
         try {
@@ -107,56 +107,35 @@ public class LabelSupportedObjectNameFactory implements ObjectNameFactory {
     }
 
     /**
-     * Returns {@code true} when metric name is a labeled metric name.
-     */
-    private boolean hasLabel(String name) {
-        return name.lastIndexOf(']') == name.length() - 1
-                && name.indexOf('[') >= 1
-                && name.indexOf('[') == name.lastIndexOf('[')
-                && name.indexOf(']') == name.lastIndexOf(']');
-    }
-
-    /**
      * Extracts the name from a labeled metric name.
      */
     private String extractMetricName(String labeledMetricName) {
-        if (!hasLabel(labeledMetricName)) {
+        if (!LabeledMetric.hasLabel(labeledMetricName)) {
             return labeledMetricName;
         }
         return labeledMetricName.substring(0, labeledMetricName.indexOf("["));
     }
 
     /**
-     * Extracts the labels from a labeled metric name. Note: Spaces are significant everywhere in an Object Name. Do not
+     * Extract and quotes values of label if required. If metricName is not a labeled metric, the returned value
+     * has a leading ',' for easier concatenation to other parts of ObjectName.
+     *
+     * Note: Spaces are significant everywhere in an Object Name. Do not
      * write "metric_name[type=Thread, name=DGC] (with a space after the comma) because it will be interpreted as having
      * a key called " name", with a leading space in the name.
      */
-    private String extractLabels(String labeledMetricName) {
-        if (!hasLabel(labeledMetricName)) {
+    private String extractQuotedLabelValues(String metricName) {
+        Map<String, String> labels = LabeledMetric.extractLabels(metricName);
+        if (labels.isEmpty()) {
             return "";
         }
 
-        return labeledMetricName.substring(
-                labeledMetricName.indexOf("[") + 1, labeledMetricName.lastIndexOf("]"));
-    }
-
-    /**
-     * Quotes values of label if required. If metricLabels is not blank, the returned value has a leading ',' for easier
-     * concatenation to other parts of ObjectName
-     */
-    private String quoteLabelValues(String metricLabels) {
-        if (metricLabels == null || metricLabels.length() == 0) {
-            return "";
-        }
-        StringBuilder labelBuilder = new StringBuilder(metricLabels.length()+1);
-        for (String label : metricLabels.split(",")) {
-            String[] labelParts = label.split("=");
-            if (labelParts.length != 2) {
-                throw new AssertionError("Illegal label provided: " + label);
-            }
+        StringBuilder labelBuilder = new StringBuilder(metricName.length()+1);
+        for (Map.Entry<String, String> label : labels.entrySet()) {
             labelBuilder.append(',');
-            labelBuilder.append(labelParts[0]).append('=').append(quoteValueIfRequired(labelParts[1]));
+            labelBuilder.append(label.getKey()).append('=').append(quoteValueIfRequired(label.getValue()));
         }
+
         return labelBuilder.toString();
     }
 }
--- src/main/java/ir/sahab/dropwizardmetrics/LabeledMetric.java
+++ src/main/java/ir/sahab/dropwizardmetrics/LabeledMetric.java
@@ -1,6 +1,9 @@
 package ir.sahab.dropwizardmetrics;
 
 import com.codahale.metrics.MetricRegistry;
+
+import java.util.Collections;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 /**
@@ -9,7 +12,7 @@ import java.util.Map;
  * You can create a labeled metric name easily by using this class.
  */
 public class LabeledMetric {
-    private StringBuilder nameAndLabels;
+    private final StringBuilder nameAndLabels;
     private boolean hasLabel;
 
     private LabeledMetric(String metricName) {
@@ -42,6 +45,39 @@ public class LabeledMetric {
         }
         return this;
     }
+
+    /**
+     * Returns {@code true} when metric name is a labeled metric name.
+     */
+    public static boolean hasLabel(String name) {
+        return name.lastIndexOf(']') == name.length() - 1
+                && name.indexOf('[') >= 1
+                && name.indexOf('[') == name.lastIndexOf('[')
+                && name.indexOf(']') == name.lastIndexOf(']');
+    }
+
+    /**
+     * Returns map of label name to label value. If metric is not a labeled one, it returns empty map.
+     */
+    public static Map<String, String> extractLabels(String name) {
+        if (!hasLabel(name)) {
+            return Collections.emptyMap();
+        }
+
+        String labelString = name.substring(name.indexOf('[') + 1, name.lastIndexOf(']'));
+
+        final Map<String, String> labels = new LinkedHashMap<>();
+        for (String label : labelString.split(",")) {
+            final String[] keyValue = label.split("=");
+            if (keyValue.length == 2) {
+                labels.put(keyValue[0], keyValue[1]);
+            } else {
+                throw new AssertionError("Invalid metric name provided: " + name);
+            }
+        }
+        return labels;
+    }
+
 
     @Override
     public String toString() {
